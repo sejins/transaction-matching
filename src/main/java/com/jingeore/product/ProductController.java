40575514fe04f90f03efc149d9bdee99c72fa81e
sejin <pengang1011@naver.com>
2021-08-21 16:47:36 +0900

    상품 등록 폼 화면

---
+++ src/main/java/com/jingeore/product/ProductController.java
@@ -0,0 +1,20 @@
+package com.jingeore.product;
+
+import com.jingeore.account.Account;
+import com.jingeore.account.CurrentUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class ProductController {
+
+    @GetMapping("/new-product")
+    public String newProductForm(@CurrentUser Account account, Model model){
+        model.addAttribute(account);
+        model.addAttribute(new ProductForm());
+        return "product/form";
+    }
+}
---
+++ src/main/java/com/jingeore/product/ProductForm.java
@@ -0,0 +1,13 @@
+package com.jingeore.product;
+
+import lombok.Data;
+
+@Data
+public class ProductForm {
+
+    private String title;
+
+    private String description;
+
+    private Long offerPrice;
+}
